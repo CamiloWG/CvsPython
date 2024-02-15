d1e6d1c1c02a600b650912dd0ad09beaa2e1433d
Camilo Madero <smadero@wi-mobile.com>
2024-02-15 17:57:55 -0500

    Refactor timing code in factorial.py to use timeit module

--- factorial.py
+++ factorial.py
@@ -1,4 +1,4 @@
-import time
+import timeit
 
 def factorial_iterativo(n):
     factorial = 1
@@ -12,21 +12,17 @@ def factorial_recursivo(n):
     else:
         return n * factorial_recursivo(n - 1)
 
-# Pedir al usuario el número para calcular su factorial
 n = int(input("Ingrese un número para calcular su factorial: "))
 
-# Calcular factorial de forma iterativa y medir tiempo
-inicio_iterativo = time.time()
-res_iterativo = factorial_iterativo(n)
-fin_iterativo = time.time()
-tiempo_iterativo = (fin_iterativo - inicio_iterativo) / 1000
+tiempo_iterativo_segundos = timeit.timeit(lambda: factorial_iterativo(n), number=1)
+tiempo_iterativo_microsegundos = tiempo_iterativo_segundos * 1e6 
+res_iterativo = factorial_iterativo(n);
 
 # Calcular factorial de forma recursiva y medir tiempo
-inicio_recursivo = time.time()
+tiempo_recursivo_segundos = timeit.timeit(lambda: factorial_iterativo(n), number=1)
+tiempo_recursivo_microsegundos = tiempo_recursivo_segundos * 1e6  
 res_recursivo = factorial_recursivo(n)
-fin_recursivo = time.time()
-tiempo_recursivo = (fin_recursivo - inicio_recursivo) / 1000
 
 print("Resultado del factorial de", n, "igual a: Recursivo =", res_recursivo, " Iterativo:", res_iterativo)
-print(f"Factorial de {n} calculado de forma iterativa en {tiempo_iterativo:.9f} milisegundos.")
-print(f"Factorial de {n} calculado de forma recursiva en {tiempo_recursivo:.9f} milisegundos.")
+print(f"Factorial de {n} calculado de forma iterativa en {tiempo_iterativo_microsegundos:.6f} microsegundos.")
+print(f"Factorial de {n} calculado de forma recursiva en {tiempo_recursivo_microsegundos:.6f} microsegundos.")
